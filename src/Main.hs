82edc20ca548163f48cecc7e77b7cf43ce5105f5
Jacob Stanley <jacob@stanley.io>
2013-04-02 23:58:59 +0800

    Switched to Win32-services package

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,71 @@
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
+
 module Main (main) where
 
 import Control.Concurrent
-import Data.IORef
-import System.Win32.Service
+import System.Win32.SystemServices.Services
+import System.Win32.Types
 
 main :: IO ()
 main = do
-    (loop, stop) <- loopStop go
-    service "sudo" loop stop
-  where
-    go = do appendFile "c:\\test.txt" "whee"
-            threadDelay 1000000
+    gState <- newMVar (1, SERVICE_STATUS WIN32_OWN_PROCESS
+                          START_PENDING [] nO_ERROR 0 0 3000)
+    mStop <- newEmptyMVar
+    startServiceCtrlDispatcher "sudo4win" 3000
+        (svcCtrlHandler mStop gState)
+        (svcMain        mStop gState)
 
-loopStop :: IO () -> IO (IO (), IO ())
-loopStop once = do
-    ref <- newIORef False
-    let stop = writeIORef ref True
-        loop = do done <- readIORef ref
-                  if done then return ()
-                          else once >> loop
-    return (loop, stop)
+
+svcMain :: MVar () -> MVar (DWORD, SERVICE_STATUS) -> ServiceMainFunction
+svcMain mStop gState _ _ h = do
+    reportSvcStatus h RUNNING nO_ERROR 0 gState
+    appendFile "c:\\test.txt" "1"
+    threadDelay 1000000
+    appendFile "c:\\test.txt" "2"
+    threadDelay 1000000
+    appendFile "c:\\test.txt" "3"
+    threadDelay 1000000
+    appendFile "c:\\test.txt" "4"
+    threadDelay 1000000
+    appendFile "c:\\test.txt" "5"
+    takeMVar mStop
+    threadDelay 1000000
+    appendFile "c:\\test.txt" "."
+    threadDelay 1000000
+    appendFile "c:\\test.txt" "."
+    threadDelay 1000000
+    appendFile "c:\\test.txt" "."
+    threadDelay 1000000
+    appendFile "c:\\test.txt" "Stahp\n"
+    reportSvcStatus h STOPPED nO_ERROR 0 gState
+
+reportSvcStatus :: HANDLE -> SERVICE_STATE -> DWORD -> DWORD
+    -> MVar (DWORD, SERVICE_STATUS) -> IO ()
+reportSvcStatus hStatus state win32ExitCode waitHint gState = do
+    modifyMVar_ gState $ \(checkPoint, svcStatus) -> do
+        let state' = nextState (checkPoint, svcStatus
+             { win32ExitCode = win32ExitCode
+             , waitHint      = waitHint
+             , currentState  = state })
+        setServiceStatus hStatus (snd state')
+        return state'
+
+nextState :: (DWORD, SERVICE_STATUS) -> (DWORD, SERVICE_STATUS)
+nextState (checkPoint, svcStatus) = case (currentState svcStatus) of
+    START_PENDING -> (checkPoint + 1, svcStatus
+        { controlsAccepted = [], checkPoint = checkPoint + 1 })
+    RUNNING -> (checkPoint, svcStatus
+        { controlsAccepted = [ACCEPT_STOP], checkPoint = 0 })
+    STOPPED -> (checkPoint, svcStatus
+        { controlsAccepted = [], checkPoint = 0 })
+    _ -> (checkPoint + 1, svcStatus
+        { controlsAccepted = [], checkPoint = checkPoint + 1 })
+
+svcCtrlHandler :: MVar () -> MVar (DWORD, SERVICE_STATUS) -> HandlerFunction
+svcCtrlHandler mStop gState hStatus STOP = do
+    reportSvcStatus hStatus STOP_PENDING nO_ERROR 3000 gState
+    putMVar mStop ()
+    return True
+svcCtrlHandler _ _ _ INTERROGATE = return True
+svcCtrlHandler _ _ _ _  = return False
